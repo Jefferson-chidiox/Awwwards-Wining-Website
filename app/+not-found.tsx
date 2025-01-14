88809f5af6affcb2d698d7f1e8eb8a0440242398
Jefferson Nwokoro <Jeffersonchidiox@gmail.com>
2025-01-14 14:44:20 +0100

    Added Photo and buttons

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -8,7 +8,7 @@ export default function NotFoundScreen() {
 
         <Stack.Screen options={{title: 'Oops Not found'}}/>
         <View style={styles.container}>
-            <Link href='/' style={styles.button}>
+            <Link href="./" style={styles.button}>
                 Go back to the Home screen
             </Link>
         </View>
